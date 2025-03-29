95564baf862a9c1584f1de490d00a5325d7a41d9
Mr1pal <91012738+Mr1pal@users.noreply.github.com>
2025-03-29 22:15:32 +0530

    Add lib/routes.dart 1743266731440

---
+++ lib/routes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/features/auth/login_screen.dart';
+import 'package:spotify_clone/features/home/home_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+        body: const Center(child: Text('Page not found')),
+      );
+    });
+  }
+}
